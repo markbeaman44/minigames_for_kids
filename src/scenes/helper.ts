7abe3cf31b8cbdf763ceb9eb008f2d9aac05bb22
Mark Beaman <30801986+markbeaman44@users.noreply.github.com>
2025-05-01 12:45:40 +0100

    fix: tablet responsive (#9)

--- src/scenes/helper.ts
+++ src/scenes/helper.ts
@@ -17,18 +17,18 @@ export function getScreenSize(width: number, height: number) {
             scaleFactorY = 0.5;
         } 
         // Landscape
-        else if (width < 950 && height < 800) {
+        else if (width < 1050 && height < 800) {
             scaleFactorX = 0.5;
             scaleFactorY = 0.45;
-        } else if (width >= 950 && width < 1050 && height < 1000) {
-            scaleFactorX = 0.55;
-            scaleFactorY = 0.5;
-        } else if (width >= 1050 && width < 1250 && height < 1000) {
-            scaleFactorX = 0.6;
-            scaleFactorY = 0.55;
+        } else if (width >= 1050 && width < 1150 && height < 1000) {
+            scaleFactorX = 0.5;
+            scaleFactorY = 0.48;
+        } else if (width >= 1150 && width < 1250 && height < 1000) {
+            scaleFactorX = 0.7;
+            scaleFactorY = 0.65;
         } else if (width >= 1250 && width < 1350 && height < 1000) {
             scaleFactorX = 0.75;
-            scaleFactorY = 0.6;
+            scaleFactorY = 0.65;
         } else if (width >= 1350 && width < 1550 && height < 1100) {
             scaleFactorX = 0.8;
             scaleFactorY = 0.75;
